80e1aee3a54d655a5a97c7de7d329cc7a6c58d5d
SHUBHANGINI SHARMA <SHUBHANGINISHARMA67@gmail.com>
2023-07-08 01:25:24 +0530

    first

--- src/SideNavbar.js
+++ src/SideNavbar.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import { Avatar, List, ListItem, ListItemIcon, ListItemText } from '@mui/material';
 import { Person as PersonIcon, Home as HomeIcon, Work as WorkIcon } from '@mui/icons-material';
-
+import { deepPurple } from '@mui/material/colors';
+import WbCloudyIcon from '@mui/icons-material/WbCloudy';
 const SideNavbar = () => {
     return (
       <nav>
         <div className="list-container">
-          <List>
+        <List>
+            <ListItem button>
+              <ListItemIcon>
+                <WbCloudyIcon sx={{ fontSize: 40 }} color="primary" />
+              </ListItemIcon>
+
+            </ListItem>
+     
             <ListItem button>
               <ListItemIcon>
                 <HomeIcon />
@@ -46,7 +54,7 @@ const SideNavbar = () => {
           </List>
         </div>
         <div className="profile-icon">
-          <Avatar>
+          <Avatar sx={{ bgcolor: deepPurple[500] }}>
             DK
           </Avatar>
         </div>
